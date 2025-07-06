8217b983da9001899951916dea3a8ac8a3cb2026
MxS3 <elsanea2019@gmail.com>
2025-07-06 20:01:47 +0300

    Update Animeluxe.js

--- Animeluxe.js
+++ Animeluxe.js
@@ -18,19 +18,24 @@ async function fetchAndSearch(keyword) {
 
 function searchResults(html) {
     const results = [];
-    const itemRegex = /<div class="col-12 col-s-6 col-m-4 col-l-3 media-block">([\s\S]*?)<\/div>\s*<\/div>/g;
+
+    const itemRegex = /<div class="col-12 col-s-6 col-m-4 col-l-3 media-block">([\s\S]*?)<\/div>/g;
     const items = html.match(itemRegex) || [];
+
     items.forEach((itemHtml) => {
         const hrefMatch = itemHtml.match(/<a[^>]+href="([^"]+)"[^>]*class="image lazyactive"/);
         const imgMatch = itemHtml.match(/data-src="([^"]+)"/);
         const titleMatch = itemHtml.match(/<h3>(.*?)<\/h3>/);
+
         const href = hrefMatch ? hrefMatch[1].trim() : '';
         const image = imgMatch ? imgMatch[1].trim() : '';
         const title = titleMatch ? decodeHTMLEntities(titleMatch[1].trim()) : '';
+
         if (href && image && title) {
             results.push({ title, href, image });
         }
     });
+
     return results;
 }
 
